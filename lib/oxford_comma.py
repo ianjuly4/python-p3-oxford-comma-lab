3f558a531c766eacda3ad4d6fac29cc43ee44b81
Ian July <ianjuly4@gmail.com>
2024-03-19 14:22:04 -0600

    oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        one_person =  "".join(items)
+        return one_person
+   
+    elif len(items) == 2:
+        two_people = " and ".join(items)
+        return two_people
+    
+    elif len(items) >= 3:
+        three_people = ", ".join(items[:-1])
+        return f'{three_people}, and {items[-1]}'
